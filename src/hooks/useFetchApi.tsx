a0d978e0768c139b071838db6b3d49c0a15caf70
Carlos Hernandez <carloshinzunza2@gmail.com>
2024-06-09 01:00:00 -0600

    added url params

--- src/hooks/useFetchApi.tsx
+++ src/hooks/useFetchApi.tsx
@@ -10,9 +10,19 @@ export const useFetch = () => {
     const fetchPokemons = async () => {
       setLoading(true);
       try {
-        const response = await fetch(
-          "https://pokedexback.onrender.com/api/pokemons"
-        );
+        const url = new URL("https://pokedexback.onrender.com/api/pokemons");
+        const urlParams = new URLSearchParams(window.location.search);
+        const limit = urlParams.get("limit");
+        const page = urlParams.get("page");
+        if (limit) {
+          url.searchParams.append("limit", limit);
+        }
+
+        if (page) {
+          url.searchParams.append("page", page);
+        }
+
+        const response = await fetch(url);
         if (!response.ok) {
           throw new Error("Failed to fetch pokemons");
         }
